b04b6deeee7c36acbc7bbfc9ee5a4ecde0bef64a
etah-major <epiegevisetah@gmail.com>
2022-09-11 10:11:43 -0700

    my printf commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
+* main - print the string in the put function
 *
-*main - print the string in the put function
-*Description: using the main function
+* Description: using the main function
 * this program prints with proper grammar, but the outcome is a piece of art
 * Return: 0
 */
-int main(void)
+int main(viod)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
